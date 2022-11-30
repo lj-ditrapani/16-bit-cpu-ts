e5e8fa0adf4eddb27fe3c04376eb678ae97e42f9
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2022-11-30 09:04:57 -0500

    Disable noUncheckedIndexedAccess in tsconfig
    This rule doesn't make sense for this project, since a lot of operations
    index into an array.  Memory is modeled as an array.  So is the register
    file.  So every cpu instruction involves indexing into an array.

--- src/ljd_16_bit_cpu.ts
+++ src/ljd_16_bit_cpu.ts
@@ -83,7 +83,7 @@ export class Cpu implements ICpu {
   }
 
   public run(n: number): Uint16Array {
-    this.instructionCounter = this.ioRam[0] as number
+    this.instructionCounter = this.ioRam[0]
     while (n > 0) {
       --n
       const done = this.step()
@@ -92,12 +92,12 @@ export class Cpu implements ICpu {
       }
     }
     if (this.activeBuffer === 1) {
-      this.ioRam2[0] = this.ioRam1[0] as number
+      this.ioRam2[0] = this.ioRam1[0]
       this.activeBuffer = 2
       this.ioRam = this.ioRam2
       return this.ioRam1
     } else {
-      this.ioRam1[0] = this.ioRam2[0] as number
+      this.ioRam1[0] = this.ioRam2[0]
       this.activeBuffer = 1
       this.ioRam = this.ioRam1
       return this.ioRam2
@@ -110,35 +110,35 @@ export class Cpu implements ICpu {
       case 'end':
         return true
       case 'hby': {
-        const value = this.registers[instruction.destinationRegister] as number
+        const value = this.registers[instruction.destinationRegister]
         this.registers[instruction.destinationRegister] =
           (instruction.immediate8Bit << 8) | (value & 0x00ff)
         this.incInstructionCounter()
         return false
       }
       case 'lby': {
-        const value = this.registers[instruction.destinationRegister] as number
+        const value = this.registers[instruction.destinationRegister]
         this.registers[instruction.destinationRegister] =
           (value & 0xff00) | instruction.immediate8Bit
         this.incInstructionCounter()
         return false
       }
       case 'lod': {
-        const address = this.registers[instruction.sourceRegister1] as number
+        const address = this.registers[instruction.sourceRegister1]
         this.registers[instruction.destinationRegister] = this.read(address)
         this.incInstructionCounter()
         return false
       }
       case 'str': {
-        const address = this.registers[instruction.sourceRegister1] as number
-        this.write(address, this.registers[instruction.sourceRegister2] as number)
+        const address = this.registers[instruction.sourceRegister1]
+        this.write(address, this.registers[instruction.sourceRegister2])
         this.incInstructionCounter()
         return false
       }
       case 'add':
         this.basicAdd(
-          this.registers[instruction.sourceRegister1] as number,
-          this.registers[instruction.sourceRegister2] as number,
+          this.registers[instruction.sourceRegister1],
+          this.registers[instruction.sourceRegister2],
           0,
           instruction.destinationRegister,
         )
@@ -146,8 +146,8 @@ export class Cpu implements ICpu {
         return false
       case 'sub':
         this.basicAdd(
-          this.registers[instruction.sourceRegister1] as number,
-          ~(this.registers[instruction.sourceRegister2] as number),
+          this.registers[instruction.sourceRegister1],
+          ~this.registers[instruction.sourceRegister2],
           1,
           instruction.destinationRegister,
         )
@@ -155,7 +155,7 @@ export class Cpu implements ICpu {
         return false
       case 'adi':
         this.basicAdd(
-          this.registers[instruction.sourceRegister1] as number,
+          this.registers[instruction.sourceRegister1],
           instruction.sourceRegister2,
           0,
           instruction.destinationRegister,
@@ -164,7 +164,7 @@ export class Cpu implements ICpu {
         return false
       case 'sbi':
         this.basicAdd(
-          this.registers[instruction.sourceRegister1] as number,
+          this.registers[instruction.sourceRegister1],
           ~instruction.sourceRegister2,
           1,
           instruction.destinationRegister,
@@ -173,30 +173,29 @@ export class Cpu implements ICpu {
         return false
       case 'and':
         this.registers[instruction.destinationRegister] =
-          (this.registers[instruction.sourceRegister1] as number) &
-          (this.registers[instruction.sourceRegister2] as number)
+          this.registers[instruction.sourceRegister1] &
+          this.registers[instruction.sourceRegister2]
         this.incInstructionCounter()
         return false
       case 'orr':
         this.registers[instruction.destinationRegister] =
-          (this.registers[instruction.sourceRegister1] as number) |
-          (this.registers[instruction.sourceRegister2] as number)
+          this.registers[instruction.sourceRegister1] |
+          this.registers[instruction.sourceRegister2]
         this.incInstructionCounter()
         return false
       case 'xor':
         this.registers[instruction.destinationRegister] =
-          (this.registers[instruction.sourceRegister1] as number) ^
-          (this.registers[instruction.sourceRegister2] as number)
+          this.registers[instruction.sourceRegister1] ^
+          this.registers[instruction.sourceRegister2]
         this.incInstructionCounter()
         return false
       case 'not':
-        this.registers[instruction.destinationRegister] = ~(this.registers[
-          instruction.sourceRegister1
-        ] as number)
+        this.registers[instruction.destinationRegister] =
+          ~this.registers[instruction.sourceRegister1]
         this.incInstructionCounter()
         return false
       case 'shf': {
-        const value = this.registers[instruction.sourceRegister1] as number
+        const value = this.registers[instruction.sourceRegister1]
         this.carryFlag = getShiftCarry(value, instruction.direction, instruction.amount)
         this.registers[instruction.destinationRegister] =
           instruction.direction === 'left'
@@ -206,7 +205,7 @@ export class Cpu implements ICpu {
         return false
       }
       case 'brv': {
-        const value = this.registers[instruction.sourceRegister1] as number
+        const value = this.registers[instruction.sourceRegister1]
         let jump = false
         if (instruction.negative && isNegative(value)) {
           jump = true
@@ -218,7 +217,7 @@ export class Cpu implements ICpu {
           jump = false
         }
         if (jump) {
-          this.instructionCounter = this.registers[instruction.sourceRegister2] as number
+          this.instructionCounter = this.registers[instruction.sourceRegister2]
         } else {
           this.incInstructionCounter()
         }
@@ -234,7 +233,7 @@ export class Cpu implements ICpu {
           jump = false
         }
         if (jump) {
-          this.instructionCounter = this.registers[instruction.sourceRegister2] as number
+          this.instructionCounter = this.registers[instruction.sourceRegister2]
         } else {
           this.incInstructionCounter()
         }
@@ -245,11 +244,11 @@ export class Cpu implements ICpu {
 
   private read(address: number): number {
     if (address < 0x8000) {
-      return this.dataRom[address] as number
+      return this.dataRom[address]
     } else if (address < 0xf800) {
-      return this.dataRam[address & 0x7fff] as number
+      return this.dataRam[address & 0x7fff]
     } else if (address < 0xfc00) {
-      return this.ioRam[address & 0x03ff] as number
+      return this.ioRam[address & 0x03ff]
     } else {
       throw new Error(
         'LJD Cpu: Tried to read from a memory address out of bounds ' + address,
